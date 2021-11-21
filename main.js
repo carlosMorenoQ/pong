7a8cd96df264d717f6014ad59b01e14a8a056046
carlosMorenoQ <92289007+carlosMorenoQ@users.noreply.github.com>
2021-11-20 20:35:08 -0500

    javaScrip MVC parte 5

--- main.js
+++ main.js
@@ -6,31 +6,42 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars;
-            // var elements = this.bars.map(function (bar) { return bar; });
+            var elements = this.bars.map(function (bar) {
+                return bar;
+            });
             elements.push(this.ball);
             return elements;
         }
     }
 })();
 
-(function (){
-    self.Ball = function (x,y,radius,board){
+(function () {
+    self.Ball = function (x, y, radius, board) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.speed_y =0;
-        this.speed_x=3;
+        this.speed_y = 0;
+        this.speed_x = 3;
         this.board = board;
-        board.ball  = this;
+        this.direction = 1;
+
+        board.ball = this;
         this.kind = "circle";
     }
-})();
 
+    self.Ball.prototype = {
+        move: function () {
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        },
+    }
+
+})();
 
 (function () {
     self.Bar = function (x, y, width, height, board) {
@@ -77,26 +88,26 @@
                     draw(this.ctx, el);
                 }
             },
-            play: function (){
-                this.clean();
-                this.draw();
+            play: function () {
+                if(this.board.playing) {
+                    this.clean();
+                    this.draw();
+                    this.board.ball.move();
+                }
             }
         }
 
         function draw(ctx, element) {
-
             switch (element.kind) {
                 case "rectangle":
                     ctx.fillRect(element.x, element.y, element.width, element.height);
                     break;
                 case "circle":
                     ctx.beginPath();
-                    ctx.arc(element.x,element.y,element.radius,0,7);
-                    ctx.fill;
+                    ctx.arc(element.x, element.y, element.radius, 0, 7);
+                    ctx.fill();
                     ctx.closePath();
                     break;
-
-
             }
         }
     }
@@ -107,23 +118,28 @@ var bar = new Bar(0, 100, 40, 100, board);
 var bar_2 = new Bar(760, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
-var ball = new Ball(350,100,10,board);
-
+var ball = new Ball(400, 200, 10, board);
 
 document.addEventListener("keydown", function (ev) {
-    ev.preventDefault();
     if (ev.keyCode === 38) {
+        ev.preventDefault();
         bar.up();
     } else if (ev.keyCode === 40) {
+        ev.preventDefault();
         bar.down();
     } else if (ev.keyCode === 87) {
+        ev.preventDefault();
         bar_2.up();
     } else if (ev.keyCode === 83) {
+        ev.preventDefault();
         bar_2.down();
+    } else if (ev.keyCode === 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 })
 
-//self.addEventListener("load", main);
+board_view.draw();
 window.requestAnimationFrame(controller);
 
 function controller() {
