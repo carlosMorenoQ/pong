3896b8b1f660fd1a0d64fa3dbef968ce4e882c03
carlosMorenoQ <92289007+carlosMorenoQ@users.noreply.github.com>
2021-11-20 20:58:56 -0500

    javaScrip MVC parte 6 y final

--- main.js
+++ main.js
@@ -8,7 +8,6 @@
         this.ball = null;
         this.playing = false;
     }
-
     self.Board.prototype = {
         get elements() {
             var elements = this.bars.map(function (bar) {
@@ -29,16 +28,37 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         board.ball = this;
         this.kind = "circle";
     }
-
     self.Ball.prototype = {
         move: function () {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
         },
+        get width() {
+            return this.radius * 2;
+        },
+        get height() {
+            return this.radius * 2;
+        },
+        collision:function (bar){
+            // Reacciona a la colisión con una barra que recibe como parametro
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
+        }
     }
 
 })();
@@ -54,7 +74,6 @@
         this.speed = 10;
         this.board.bars.push(this);
     }
-
     self.Bar.prototype = {
         down: function () {
             this.y += this.speed;
@@ -69,7 +88,6 @@
 })();
 
 (function () {
-
         self.BoardView = function (canvas, board) {
             this.canvas = canvas;
             this.canvas.width = board.width;
@@ -77,7 +95,6 @@
             this.board = board;
             this.ctx = canvas.getContext("2d");
         }
-
         self.BoardView.prototype = {
             clean: function () {
                 this.ctx.clearRect(0, 0, this.board.width, this.board.height);
@@ -88,15 +105,44 @@
                     draw(this.ctx, el);
                 }
             },
+            check_collisions: function(){
+                for (var i = this.board.bars.length-1; i >=0 ; i--) {
+                    var bar = this.board.bars[i];
+                    if(hit(bar,this.board.ball)){
+                        this.board.ball.collision(bar);
+                    }
+                }
+            },
             play: function () {
                 if(this.board.playing) {
                     this.clean();
                     this.draw();
+                    this.check_collisions();
                     this.board.ball.move();
                 }
             }
         }
-
+        function hit(a, b) {
+            //Revisa si a colisiona con b
+            var hit = false;
+            //Colsiones horizontales
+            if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+                //Colisiones verticales
+                if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                    hit = true;
+            }
+            //Colisión de a con b
+            if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+                if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+                    hit = true;
+            }
+            //Colisión b con a
+            if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+                if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                    hit = true;
+            }
+            return hit;
+        }
         function draw(ctx, element) {
             switch (element.kind) {
                 case "rectangle":
